849361077b21e047f906aaa888f6e4855bcacecd
milos-savic <savic.losmi@gmail.com>
2017-09-30 19:12:46 +0200

    cli client start

--- dist/cli-app-adapter/src/main/java/by/modus/percentilerank/PercentileRankCalcApp.java
+++ dist/cli-app-adapter/src/main/java/by/modus/percentilerank/PercentileRankCalcApp.java
@@ -1,18 +1,15 @@
 package by.modus.percentilerank;
 
+import by.modus.percentilerank.facade.PercentileRankCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class PercentileRankCalcApp {
 
     private final static Logger log = LoggerFactory.getLogger(PercentileRankCalcApp.class);
 
-    /**
-     * Main method, used to run the application.
-     *
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-
     }
 }
